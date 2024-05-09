74e5a717a1d6646faa3122ca0a88a344247d21ce
seanwlk <nicola_97@rocketmail.com>
2024-05-09 21:38:29 +0200

    refactor warnings command and increased character limit

--- commands/warnings.js
+++ commands/warnings.js
@@ -17,16 +17,19 @@ module.exports = {
 			.setColor('#ff0000')
 			.setTitle(`${args[0]} has ${warnings.length} warnings`);
 		var embedBody = "";
-		Object.keys(warnings).forEach(function(key) {
-			var warning = warnings[key];
-			let warnDate = new Date(parseInt(warning.date * 1000)).toLocaleDateString("it-IT");
-			embedBody += `Mod <@${warning.moderator}> | Reason '${warning.reason}' | Date ${warnDate}\n`;
-		});
-		if (warnings.length === 0) embedBody = "No warnings for this user";
-		if (embedBody.length < 1023) {
-			warningsEmbed.addFields({name :`These are the warnings for this user`, value: embedBody, inline: true});
+		if (warnings.length === 0){
+			embedBody = "No warnings for this user";
 		} else {
-			warningsEmbed.addFields({name :`Too many warnings`, value: `Please use the [web dashboard](${config.webapp_url}/warnings.php?user=${args[0]}) to check warnings for this user.`, inline: true});
+			Object.keys(warnings).forEach(function(key) {
+				var warning = warnings[key];
+				let warnDate = new Date(parseInt(warning.date * 1000)).toLocaleDateString("it-IT");
+				embedBody += `Mod <@${warning.moderator}> | Reason '${warning.reason}' | Date ${warnDate}\n`;
+			});
+		}
+		if (embedBody.length < 3900) {
+			warningsEmbed.setDescription(embedBody);
+		} else {
+			warningsEmbed.setDescription(`Please use the [web dashboard](${config.webapp_url}/warnings.php?user=${args[0]}) to check warnings for this user.`);
 		}
 		message.channel.send({embeds:[warningsEmbed]});
 	},
